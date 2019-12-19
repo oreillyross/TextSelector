51dc5559f42c56aa710f35085dbfa3ac06c01803
Faktor 10 <oreillyross@gmail.com>
2019-12-19 10:44:21 +0100

    add another test

--- src/TextSelector.js
+++ src/TextSelector.js
@@ -1,35 +1,28 @@
-import React  from "react";
+import React from "react";
 import { TextArea } from "semantic-ui-react";
+import TagDisplay from "./TagDisplay";
 
 const TextSelector = ({ article, selectText }) => {
-  
-  const {
-    title,
-    source,
-    publishedDateTime,
-    url,
-    text
-   } = article;
-
-
+  const { title, source, publishedDateTime, url, text, initialTags } = article;
+  console.log(initialTags.length);
   return (
-    <div style={{padding: '1.2rem'}}>
+    <div style={{ padding: "1.2rem" }}>
       <a href={url} rel="noreferrer noopener" target="_blank">
-      <h1>{title}</h1>
-            </a>
-      
+        <h1>{title}</h1>
+      </a>
+
       <h2>
-        by the {source}{" "}
-        <span> published on: {publishedDateTime}</span>{" "}
+        by the {source} <span> published on: {publishedDateTime}</span>{" "}
       </h2>
 
       <div style={{ textAlign: "center" }}>
         <TextArea
           onMouseUp={() => selectText(window.getSelection().toString())}
           rows={20}
-          style={{ width: "90%", padding: '1.2rem' }}
+          style={{ width: "90%", padding: "1.2rem" }}
           value={text}
         />
+        <TagDisplay tags={initialTags} />
       </div>
     </div>
   );
